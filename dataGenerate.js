6adebd10552ecb0dbc9b398f8b65d6a5d16b71d5
ayush2002-de <ayush.kumar0209@gmail.com>
2023-08-16 12:02:38 +0530

    js-assignment-v1: Generate json-data of 100 random books.
    js-assignment-v1: index update

---
+++ dataGenerate.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import short from 'short-uuid';
+
+class Book {
+  constructor() {
+    this.genres = [
+      'Mystery',
+      'Science Fiction',
+      'Fantasy',
+      'Romance',
+      'Thriller',
+      'Historical Fiction',
+      'Horror',
+      'Adventure',
+      'Non-fiction',
+      'Biography',
+      'Self-help',
+    ];
+    this.bookData = [];
+  }
+
+  generateBookData() {
+    for (let bookCount = 0; bookCount < 100; bookCount += 1) {
+      const book = {
+        bookId: short.generate(),
+        genre: this.genres[Math.floor(Math.random() * 100) % this.genres.length],
+        price: Math.floor(Math.random() * 10000),
+      };
+      this.bookData.push(book);
+    }
+    return this.bookData;
+  }
+}
+
+
+const data = JSON.stringify(new Book().generateBookData());
+fs.writeFileSync('data.json', data);
